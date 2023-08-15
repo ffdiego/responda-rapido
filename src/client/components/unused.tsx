46f17345ac0071426c7413a9cda099a8f6e948fa
Diego Fernandes <diego.silva@luz-ef.com>
2023-08-15 18:50:59 -0400

    update ruim

--- src/client/components/unused.tsx
+++ src/client/components/unused.tsx
@@ -10,7 +10,7 @@ function PlayersList() {
   return (
     <div className="flex flex-col justify-center gap-5 flex-1 my-3">
       <div className="box-border border-4 border-color3 bg-color3 mt-2 py-2 px-1 rounded-xl text-white drop-shadow-lg">
-        <h1 className="text-center">Jogadores Conectados</h1>
+        <h1 className="text-center">Jogadores Conectadossss</h1>
         {jogadores.map((jogador, index) => (
           <div
             className="flex justify-between even:bg-black even:bg-opacity-20 px-4"
